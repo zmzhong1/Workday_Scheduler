52818154d5ccd8890878cbf6d347e503be6a66d0
zmzhong1 <zhongzm8e@gmail.com>
2022-07-04 00:22:37 -0700

    time

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,18 @@
 let today = moment();
 $('#currentDay').text(today.format("dddd, MMMM Do"));
 
-let timeBlock = $(".time-block");
-let input = $("#userInputl");
-// input.addClass('past');
-
-// let timeBlock = document.querySelectorAll(".time")
-console.log(timeBlock);
-console.log(timeBlock[0].id);
+let timeBlock = $(".col-9");
+let input = $("#userInput");
 let currentHour = moment().format("H");
 console.log(currentHour);
 
 for (var i = 0; i < 9; i++) {
-    if (timeBlock[i].id < currentHour) {
-        timeBlock[i].input.addClass('past');
+    if (i + 9 < currentHour) {
+        timeBlock.eq(i).addClass('past');
+    } else if (i + 9 > currentHour) {
+        timeBlock.eq(i).addClass('future');
+    } else {
+        timeBlock.eq(i).addClass('present');
     }
 }
 
